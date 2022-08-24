aff88929b7e24d8ef840971a6dc6e2bf3fd6c5af
Marcos Antonio <MarcosAnt@users.noreply.github.com>
2022-08-24 16:49:47 -0300

    Correção para campos UUID
    Correção para geração automática de UUID

--- src/main/resources/banco/creates.sql
+++ src/main/resources/banco/creates.sql
@@ -7,27 +7,33 @@ CREATE DATABASE spring_crud
     WITH 
     OWNER = postgres
     ENCODING = 'UTF8'
-    CONNECTION LIMIT = -1; 
+    CONNECTION LIMIT = -1;
+
+-- EXTENÇÃO PARA GERAÇÃO DE UUID
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 -- TABELAS
 create table produto (
-	id UUID primary key,
+	id UUID default uuid_generate_v4(),
 	data timestamp not null,
 	percentual_desconto decimal,
-	valor decimal
+	valor decimal,
+	primary key (id)
 );
 
 create table item (
-	id UUID primary key,
+	id UUID default uuid_generate_v4(),
 	ativo boolean default true,
 	tipo_item varchar(10),
-	valor decimal
+	valor decimal,
+	primary key (id)
 );
 
 create table pedido_item (
-	id UUID primary key,
+	id UUID default uuid_generate_v4(),
 	pedido_id UUID,
 	item_id UUID,
+	primary key (id),
 	foreign key (pedido_id) references produto (id),
 	foreign key (item_id) references item (id)
 );
